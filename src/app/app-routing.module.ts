4b3dab4b9cbe64aa49e3d54c068980957678ae70
daoson12 <daoduvictor48@gmail.com>
2021-07-10 12:21:55 +0100

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,47 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CourseListComponent } from './components/courses/course-list/course-list.component';
+import { CreateCourseComponent } from './components/courses/create-course/create-course.component';
+import { CreateDepartmentComponent } from './components/department/create-department/create-department.component';
+import { DepartmentListComponent } from './components/department/department-list/department-list.component';
+import { CreateFacultyComponent } from './components/faculty/create-faculty/create-faculty.component';
+import { FacultyListComponent } from './components/faculty/faculty-list/faculty-list.component';
+import { DashboardComponent } from './layouts/dashboard/dashboard.component';
+import { HomeComponent } from './layouts/home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path:'', component:HomeComponent,
+    children:[
+      {
+        path:'', component:DashboardComponent
+      },
+      
+      {
+        path:'create-faculty', component:CreateFacultyComponent
+      },
+      {
+        path:'faculty-list', component:FacultyListComponent
+      },
+      {
+        path:'create-department', component:CreateDepartmentComponent
+      },
+      {
+        path:'department-list', component: DepartmentListComponent
+      },
+      {
+        path:'create-course', component:CreateCourseComponent
+      },
+      {
+        path:'course-list', component:CourseListComponent
+      }
+
+
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './layouts/home/home.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { NavBarComponent } from './layouts/nav-bar/nav-bar.component';
+import { DashboardComponent } from './layouts/dashboard/dashboard.component';
+import { CreateFacultyComponent } from './components/faculty/create-faculty/create-faculty.component';
+import { FacultyListComponent } from './components/faculty/faculty-list/faculty-list.component';
+import { CreateDepartmentComponent } from './components/department/create-department/create-department.component';
+import { DepartmentListComponent } from './components/department/department-list/department-list.component';
+import { CreateCourseComponent } from './components/courses/create-course/create-course.component';
+import { CourseListComponent } from './components/courses/course-list/course-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FooterComponent,
+    NavBarComponent,
+    DashboardComponent,
+    CreateFacultyComponent,
+    FacultyListComponent,
+    CreateDepartmentComponent,
+    DepartmentListComponent,
+    CreateCourseComponent,
+    CourseListComponent
   ],
   imports: [
     BrowserModule,
